89f2854e4b7f264852f122c60b66f70f4f6cc3e4
Kineruu <Kineru08@gmail.com>
2026-04-10 08:51:04 +0200

    Added format_size function

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import os
 """
 Start of the project ~19:45 CEST, 09/04/2026 (9th April 2026)
 
-TODO:
-Sort files by:
--> Size,
--> Extension
+Add _x (x being a number) if there are files with the same data (same name for example)
+Add an option to put all files into folders
 """
 
 current_path = input("Enter folder's path: ")
@@ -33,15 +31,34 @@ def sort_by_date():
     except FileNotFoundError:
         print("The file does not exist.")
 
+def format_size(size_in_bytes, option):
+    if option == 1:
+        return f"{size_in_bytes} B"
+    elif option == 2:
+        return f"{size_in_bytes / 1024:.2f} KB"
+    elif option == 3:
+        return f"{size_in_bytes / (1024 ** 2):.2f} MB"
+    elif option == 4:
+        return f"{size_in_bytes / (1024 ** 3):.2f} GB"
+    else:
+        return f"{size_in_bytes} B"
+
 def sort_by_size():
+    size_format = int(input("1. B\n2. KB\n3. MB\n4. GB\nOPTION: "))
+
+    if size_format == None:
+        size_format = 3
+
     try:
         files = os.listdir(current_path)
         for item in files:
             full_path = os.path.join(current_path, item)
+
             if os.path.isfile(full_path):
                 get_file_size = os.path.getsize(full_path)
 
-                readable_size = f"{get_file_size} bytes"
+                readable_size = format_size(get_file_size, size_format)
+
                 get_extension = os.path.splitext(item)[1]
                 new_item_name = f"{readable_size}{get_extension}"
                 new_full_path = os.path.join(current_path, new_item_name)
