50d2e343b98edb81bf33e546934c3dc3b3a411f4
Kineruu <Kineru08@gmail.com>
2026-04-10 08:35:45 +0200

    Added sort_by_size function

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ Start of the project ~19:45 CEST, 09/04/2026 (9th April 2026)
 
 TODO:
 Sort files by:
--> Date,
 -> Size,
--> Extension,
--> Name
+-> Extension
 """
 
 current_path = input("Enter folder's path: ")
@@ -30,9 +28,33 @@ def sort_by_date():
                 
                 if not os.path.exists(new_full_path):
                     os.rename(full_path, new_full_path)
+        print("Sorted.")
 
     except FileNotFoundError:
         print("The file does not exist.")
 
-sort_by_date()
+def sort_by_size():
+    try:
+        files = os.listdir(current_path)
+        for item in files:
+            full_path = os.path.join(current_path, item)
+            if os.path.isfile(full_path):
+                get_file_size = os.path.getsize(full_path)
+
+                readable_size = f"{get_file_size} bytes"
+                get_extension = os.path.splitext(item)[1]
+                new_item_name = f"{readable_size}{get_extension}"
+                new_full_path = os.path.join(current_path, new_item_name)
+                
+                if not os.path.exists(new_full_path):
+                    os.rename(full_path, new_full_path)
+        print("Sorted.")
+
+    except FileNotFoundError:
+        print("The file does not exist.")    
+
+if __name__ == "__main__":
+    get_user_option = int(input("1. Sort by date\n2. Sort by size\n3. Extension\nOPTION: "))
+    if get_user_option == 1: sort_by_date()
+    if get_user_option == 2: sort_by_size()
 
