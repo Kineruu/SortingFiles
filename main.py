8640cad63f6db0e26025c91cd80e049f8d1de4ae
Kineruu <Kineru08@gmail.com>
2026-04-11 15:56:06 +0200

    Making it slightly smaller (in terms of lines)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Start of the project ~19:45 CEST, 09/04/2026 (9th April 2026)
 
 current_path = input("Enter folder's path: ")
 
-if os.path.exists(current_path):
+if os.path.exists(current_path): 
     pass
 else:
     print("That path does not exist!")
@@ -20,10 +20,10 @@ def sort_by_date():
         files = os.listdir(current_path)
         for item in files:
             full_path = os.path.join(current_path, item)
+            
             if os.path.isfile(full_path):
                 created_time = os.path.getctime(full_path)
                 readable_time = time.strftime("%d-%m-%Y_%H-%M-%S", time.localtime(created_time))
-
                 get_extension = os.path.splitext(item)[1]
                 new_item_name = f"{readable_time}{get_extension}"
                 new_full_path = os.path.join(current_path, new_item_name)
@@ -37,27 +37,19 @@ def sort_by_date():
         print("Sorted.")
         input()
 
-    except FileNotFoundError:
-        print("The file does not exist.")
+    except FileNotFoundError: print("The file does not exist.")
 
 def format_size(size_in_bytes, option):
-    if option == 1:
-        return f"{size_in_bytes} B"
-    elif option == 2:
-        return f"{size_in_bytes / 1024:.2f} KB"
-    elif option == 3:
-        return f"{size_in_bytes / (1024 ** 2):.2f} MB"
-    elif option == 4:
-        return f"{size_in_bytes / (1024 ** 3):.2f} GB"
-    else:
-        return f"{size_in_bytes} B"
+    if option == 1: return f"{size_in_bytes} B"
+    elif option == 2: return f"{size_in_bytes / 1024:.2f} KB"
+    elif option == 3: return f"{size_in_bytes / (1024 ** 2):.2f} MB"
+    elif option == 4: return f"{size_in_bytes / (1024 ** 3):.2f} GB"
+    else: return f"{size_in_bytes} B"
 
 def sort_by_size():
     number = 1
-    try:
-        size_format = int(input("1. B\n2. KB\n3. MB\n4. GB\nOPTION: "))
-    except ValueError:
-        size_format = 3
+    try: size_format = int(input("1. B\n2. KB\n3. MB\n4. GB\nOPTION: "))
+    except ValueError: size_format = 3
 
     try:
         files = os.listdir(current_path)
@@ -68,9 +60,7 @@ def sort_by_size():
                 get_file_size = os.path.getsize(full_path)
 
                 readable_size = format_size(get_file_size, size_format)
-
                 get_extension = os.path.splitext(item)[1]
-
                 new_item_name = f"{readable_size}{get_extension}"
                 new_full_path = os.path.join(current_path, new_item_name)
                 
@@ -84,8 +74,7 @@ def sort_by_size():
         print("Sorted.")
         input()
 
-    except FileNotFoundError:
-        print("The file does not exist.")    
+    except FileNotFoundError: print("The file does not exist.")    
 
 if __name__ == "__main__":
     get_user_option = int(input("1. Sort by date\n2. Sort by size\nOPTION: "))
